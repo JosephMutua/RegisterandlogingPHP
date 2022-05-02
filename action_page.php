e848d8d540f1f520961ad04f7d5fc084e8a58cac
JosephMutua <josephmutua1738@gmail.com>
2022-05-02 22:38:47 +0300

    End of the register backend

--- action_page.php
+++ action_page.php
@@ -2,7 +2,24 @@
     session_start();
 
     $con = mysqli_connect('localhost', 'root','');
-    mysqli_select_db($con, registerandlogingphp)
+    mysqli_select_db($con, 'registerandlogingphp');
 
-    $email = $_POST[];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $password = $_POST['pswd'];
+
+    $s = "Select * from user_details where email='$email'";
+    $result = mysqli_query($con,$s);
+
+    $num = mysqli_num_rows($result);
+
+    if ($num == 1) {
+        echo "Email already Registered";
+    }
+    else {
+        $register = "Insert into user_details (fname, lname, email, password) values ('$fname','$lname','$email','$password')";
+        mysqli_query($con, $register);
+        echo "Registration Sucessful.";
+    }
 ?>
